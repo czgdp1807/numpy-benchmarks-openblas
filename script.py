32656c11558f9c295e385c22d3b373693a61b57a
Gagandeep Singh <gdp.1807@gmail.com>
2024-04-29 12:04:06 +0530

    DEV: Added utility script

---
+++ script.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Parse information needed for benchmarking.')
+    parser.add_argument('--set-commit-hash', dest='commit_hash', required=True)
+    parser.add_argument('--benchmark-name', dest='benchmark_name', default=None)
+    parser.add_argument('--hardware', dest='hardware', required=True)
+    parser.add_argument('--result-dir', dest='result_dir', required=True)
+
+    args = parser.parse_args()
+    scripts = [
+        "../numpy-benchmarks-openblas/AutoBenchOpenBLASKernels.py",
+        "../numpy-benchmarks-openblas/SimplifyASVBenchmarkResults.py",
+        "../numpy-benchmarks-openblas/CompareSimplifiedBenchmarkResults.py"
+    ]
+
+    for script in scripts:
+        cmd = "python {}".format(script)
+        cmd_args = " --set-commit-hash={} --benchmark-name={} --hardware={} --result-dir={}".format(
+                    args.commit_hash, args.benchmark_name, args.hardware, args.result_dir)
+        os.system(cmd + cmd_args)
