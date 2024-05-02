cdb1226334122ecdfab1b692ad4e2bc5fb3e3e71
Gagandeep Singh <gdp.1807@gmail.com>
2024-05-02 16:44:07 +0530

    DEV: Generate better plots

--- CompareSimplifiedBenchmarkResults.py
+++ CompareSimplifiedBenchmarkResults.py
@@ -32,7 +32,7 @@ def tabulate_results(benchmarkparams2archtimes, threshold):
                                 headers="keys", tablefmt="pipe"))
         print()
 
-def plot_results(benchmarkparams2archtimes, threshold):
+def plot_results(benchmarkparams2archtimes, hardware, threshold):
     for benchmark in benchmarkparams2archtimes:
         are_all_below_threshold = True
         for mean, spread, arch in benchmarkparams2archtimes[benchmark]:
@@ -53,16 +53,35 @@ def plot_results(benchmarkparams2archtimes, threshold):
         means = [mean for mean, _, _ in combined]
         spreads = [spread for _, spread, _ in combined]
         archs = [arch for _, _, arch in combined]
+        fig = plt.figure(figsize=(5, 5))
+        plt.subplots_adjust(bottom=0.4)
+        SMALL_SIZE = 8
+        MEDIUM_SIZE = 10
+        BIGGER_SIZE = 12
+
+        plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+        plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+        plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+        plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+        plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+        plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+        plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+        plt.xticks(rotation=90)
+        plt.title("Benchmark: {}\nArchitecture: {}".format(benchmark, hardware), fontweight="bold")
+        plt.xlabel("Target OpenBLAS Architecutre", labelpad=10, fontweight='bold')
+        plt.ylabel("Time (in s)", fontweight='bold')
         plt.errorbar(archs, means, spreads, linestyle='None', marker='^')
         plt.show()
+        fig.savefig("graphs/{}_{}.png".format(benchmark, hardware))
         print()
 
-def process_simplified_benchmark_results(target_archs, commit_hash, result_dir,
+def process_simplified_benchmark_results(target_archs, hardware, commit_hash, result_dir,
                                          benchmark_name, presentation, threshold):
     arch2benchdata = []
     for target_arch in target_archs:
         simplified_results_dir = os.path.expanduser("{}/simplified_results".format(result_dir))
         os.chdir(simplified_results_dir)
+        os.system("mkdir -p graphs")
         filename = "{}/{}.json".format(
             simplified_results_dir,
             generate_target_dir_name(commit_hash, target_arch, benchmark_name)
@@ -100,7 +119,7 @@ def process_simplified_benchmark_results(target_archs, commit_hash, result_dir,
     if presentation == "table":
         tabulate_results(benchmarkparams2archtimes, threshold)
     elif presentation == "graph":
-        plot_results(benchmarkparams2archtimes, threshold)
+        plot_results(benchmarkparams2archtimes, hardware, threshold)
     else:
         raise ValueError("{} is not supported.".format(presentation))
 
@@ -115,6 +134,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     process_simplified_benchmark_results(
-        openblas_target_cpu_archs[args.hardware], args.commit_hash, args.result_dir,
-        args.benchmark_name, args.presentation, args.threshold
+        openblas_target_cpu_archs[args.hardware], args.hardware,
+        args.commit_hash, args.result_dir, args.benchmark_name,
+        args.presentation, args.threshold
     )
